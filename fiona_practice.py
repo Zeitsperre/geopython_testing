a1e840baf6e0fb5c3e9160f64acc54afccfc0130
Trevor James Smith <trevor_smith@live.com>
2018-05-28 15:32:10 -0400

    More practice

--- fiona_practice.py
+++ fiona_practice.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Polygon, Point
 
 
-def geotesting():
+def geo_testing():
 
     p1 = Polygon([(0, 0), (0, -2), (-1, -2), (-2, -1)])
     p2 = Polygon([(0, 0), (2, 0), (2, 1.5), (0, 1)])
@@ -26,12 +26,15 @@ def geotesting():
     plt.show()
 
 
-def geopandastesting():
+def geopandas_testing() -> None:
 
     # Polygons take X,Y as parameters
     f = Polygon([(-73.4525, 45.5836), (-73.5325, 45.6336), (-73.5325, 45.5336), (-73.3325, 45.5336)])
-    fg = gpd.GeoSeries(f)
+    f = gpd.GeoSeries(f)
+    fg = gpd.GeoDataFrame({"geometry": f, "properties": None})
+    fg["properties"] = [{"color": "blue"}]
     fg.crs = {"init": "epsg:4326"}
+    print(fg)
 
     p = []
     for i in range(200):
@@ -39,25 +42,40 @@ def geopandastesting():
         y = random.uniform(45.1500, 45.6500)
         data = Point([x, y])
         p.append(data)
-    pg = gpd.GeoSeries(p)
+    p = gpd.GeoSeries(p)
+    pg = gpd.GeoDataFrame({"geometry": p, "properties": None})
     pg.crs = {"init": "epsg:4326"}
+
+    q = []
+    for i in range(200):
+        x = random.uniform(-73.2500, -73.8500)
+        y = random.uniform(45.1500, 45.6500)
+        data = Point([x, y])
+        q.append(data)
+    q = gpd.GeoSeries(q)
+    qg = gpd.GeoDataFrame({"geometry": q, "properties": None})
+    qg.crs = {"init": "epsg:4326"}
+    # qg["color"] = ["red"]
 
     joined = pg.intersection(fg.unary_union)
 
-    makefolium(fg, pg, joined)
+    make_folium(fg, pg, qg)
 
     joined.plot()
     plt.show()
 
 
-def makefolium(*args):
+def make_folium(*args):
 
     # Maps take Y,X as parameters
     m = folium.Map(location=[45.5225, -73.4525])
 
-    for poly in args:
+    colors = ["red", "yellow", "purple", "blue"]
+
+    for idx, poly in enumerate(args):
         try:
-            folium.GeoJson(poly).add_to(m)
+            m.add_child(folium.GeoJson(data=poly, style_function=lambda x:
+                {"color": "green" if x["geometry"]["type"] == "Polygon" else colors[idx]}))
         except (AttributeError, TypeError, ValueError) as e:
             print(poly, e)
             try:
@@ -72,5 +90,5 @@ def makefolium(*args):
 
 
 if __name__ == '__main__':
-    # geotesting()
-    geopandastesting()
+    # geo_testing()
+    geopandas_testing()
